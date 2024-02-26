207f7cfd2f76fe2c348c7750eaaeacd0beeb43fe
PabloGarcia123 <93787897+PabloGarcia123@users.noreply.github.com>
2024-02-26 20:04:48 +0100

    Add files via upload

---
+++ src/main/java/edu/uclm/es/sqeusers/model/User.java
@@ -0,0 +1,24 @@
+package edu.uclm.es.sqeusers.model;
+
+public class User {
+    private String email;
+    private String pwd;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+
+}
